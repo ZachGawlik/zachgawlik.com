94235e0ec9c8edc30e47a9e859a44e3a891d56f5
Zach Gawlik <ZachGawlik@users.noreply.github.com>
2019-04-03 18:31:06 -0400

    Add lightly customized gatsby starter

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: 'react-app',
+  globals: {
+    __PATH_PREFIX__: true,
+  },
+  rules: {
+    'react/display-name': 'none',
+    'jsx-a11y/accessible-emoji': 'off',
+  },
+}
