2854c031ac773d3aefac36c403c5338a93e26e71
Zach Gawlik <ZachGawlik@users.noreply.github.com>
2019-04-09 22:11:12 -0400

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,13 +61,13 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'Gatsby Starter Blog',
-        short_name: 'GatsbyJS',
+        name: 'Zach Gawlik',
+        short_name: 'Zach Gawlik',
         start_url: '/',
         background_color: '#ffffff',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'content/assets/gatsby-icon.png',
+        icon: 'src/assets/manifest-icon.svg',
       },
     },
     'gatsby-plugin-offline',
